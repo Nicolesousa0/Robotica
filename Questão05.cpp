13e229dc55aa1fe6aef5a66f5a085ab243007d50
Nicolesousa0 <nicolegit01@gmail.com>
2025-07-08 19:16:06 -0300

    Questão05.cpp

--- Questão05.cpp
+++ Questão05.cpp
@@ -1,18 +1,16 @@
-#include <iostream>
-#include <string> 
- 
- 
-int main(){
-    std:: string nome;
+#include<iostream>
+#include<string>
+
+int main() {
+    std :: string nome;
     int idade;
     
-    //nome
-    std::cout<<"informe seu nome: ";
-    std::getline(std::cin, nome); 
+    std :: cout <<"Informe seu nome: " <<std :: endl;
+    std :: getline(std::cin, nome);
     
-    //idade
-     std::cout << "informe sua idade: ";
-     std::cin >> idade;
+    std :: cout <<"Informe sua idade: " <<std :: endl;
+    std :: cin>>idade;
     
-    std::cout<<" oiii, "<<nome<<"! voce tem "<<idade<<" aninhos."<< std::endl;
+    std :: cout <<nome<<", voce possui "<<idade<<" anos." <<std :: endl;
+    
     }
