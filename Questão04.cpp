5941632c3ecb8727dbff924f0212da50a2ca82b0
Nicolesousa0 <nicolegit01@gmail.com>
2025-07-08 17:42:34 -0300

    Questão04.cpp

--- Questão04.cpp
+++ Questão04.cpp
@@ -1,20 +1,19 @@
 #include<iostream>
 
-int main(){
-    int x=5, y=2;
-    float f1=5.0, f2=2.0;
-    double d1=5.0, d2=2.0;
-    
-    //operacoes
-    
-    int resultadoInt=x/y;
-    float resultadoFloat=f1/f2;
-    double resultadoDouble=d1/d2;
-    
-     std::cout << "Divisao com int (5 / 2): " << resultadoInt <<  ("perde a parte decimal")<< std::endl;
-      std::cout << "Divisao com flout (5.0 / 2.0): " << std::fixed<< resultadoFloat << std::endl;
-      std::cout << "Divisao com double (5.0 / 2.0): "  << std::fixed<< resultadoDouble << std::endl;
-     
-    
+using namespace std;
+
+int main() {
+
+int vari1 = 10, vari2 = 2;
+float vari3 = 5.0, vari4 = 2.0;
+double vari5 = 5.0, vari6 = 3.0;
+
+int resultado_1 = vari1 / vari2;
+float resultado_2 = vari3 / vari4; 
+double resultado_3 = vari5 / vari6;
+
+cout <<"Int: "<<resultado_1<< endl;
+cout <<"Float: "<<resultado_2<< endl;
+cout <<"Double: "<<resultado_3<< endl;
 
 }
